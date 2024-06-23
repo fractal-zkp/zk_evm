1dcbf4ea3bb8bd148e606c4b0d79023eafde9eee
frisitano <francesco.risitano95@gmail.com>
2024-06-23 13:58:22 +0800

    migrate compat to micro crate

--- zero_bin/rpc/src/lib.rs
+++ zero_bin/rpc/src/lib.rs
@@ -6,10 +6,11 @@ use alloy::{
 };
 use anyhow::Context as _;
 use clap::ValueEnum;
+use compat::Compat;
 use evm_arithmetization::proof::{BlockHashes, BlockMetadata};
 use futures::{StreamExt as _, TryStreamExt as _};
 use prover::ProverInput;
-use trace_decoder::types::{BlockLevelData, Compat, OtherBlockData};
+use trace_decoder::types::{BlockLevelData, OtherBlockData};
 use zero_bin_common::block_interval::BlockInterval;
 
 pub mod jerigon;
